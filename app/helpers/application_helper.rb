6e6a1c6d3e6854767f8d8b8097298db86fc61359
Rod Wilhelmy <rwilhelmy@gmail.com>
2013-02-09 22:30:35 -0600

    Add timeago helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,9 @@ module ApplicationHelper
     { "data-longitude" => lng, "data-latitude" => lat }
   end
 
+  def timeago(time, options = {})
+    options[:class] ||= "timeago"
+    content_tag(:abbr, time.to_s, options.merge(:title => time.getutc.iso8601)) if time
+  end
+
 end
