af5560c04d263ebad1f7dd5e10c4fd9492165457
Rod Wilhelmy <rwilhelmy@gmail.com>
2013-02-20 11:42:13 -0600

    Add navigation menu for accessing the admin panel
    Redirects to root, when admin signs in
    
    Fixes #54
    Fixes #67

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -2,10 +2,10 @@ class Admins::SessionsController < Devise::SessionsController
   layout 'admins'
 
   def after_sign_in_path_for(resource)
-    admins_categories_path
+    root_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
-    new_admin_session_path
+    root_path
   end
 end
