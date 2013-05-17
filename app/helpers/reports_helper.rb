0606a68fcba70b072a4ad38f05281e787c4a7559
Javier Ayala <jav.forzza@gmail.com>
2013-05-16 19:08:36 -0500

    basic home layout

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -8,11 +8,11 @@ module ReportsHelper
     if user_signed_in? && !current_user.voted_on?(report)
       link_to 'VOTA', vote_report_path(report), :remote => true, :method => :post, :class => "js-vote_report", class: "button blue rounded"
     elsif user_signed_in? && current_user.voted_on?(report)
-      link_to 'VOTASTE', "javascript:void(0)", class: "button blue rounded"
+      link_to 'VOTASTE', "javascript:void(0)", class: "button green"
     elsif admin_signed_in?
-      link_to 'VOTA', "javascript:void(0)", { class: "button blue rounded blocked", data: { message: "Los administradores no pueden votar." } }
+      link_to 'VOTA', "javascript:void(0)", { class: "button green blocked", data: { message: "Los administradores no pueden votar." } }
     else
-      link_to 'VOTA', "javascript:void(0)", { class: "button blue rounded blocked", data: { message: "Para votar necesitas registrarte." } }
+      link_to 'VOTA', "javascript:void(0)", { class: "button green blocked", data: { message: "Para votar necesitas registrarte." } }
     end
   end
 
