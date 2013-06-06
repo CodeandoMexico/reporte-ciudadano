94f3df6ef80f4a7f3648f39265cecd49dbde85d6
Rod Wilhelmy <rwilhelmy@gmail.com>
2013-06-06 15:47:53 -0500

    Loads unicorn timeout config from ENV

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
 # config/unicorn.rb
 
 worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
-timeout 15
+timeout Integer(ENV['WEB_TIMEOUT'] || 15)
 preload_app true
 
 before_fork do |server, worker|
