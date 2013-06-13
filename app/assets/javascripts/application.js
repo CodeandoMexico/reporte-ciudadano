9d10351fe9c9848fa9a80d6618b3e6087ded2f48
Adrian Gonzalez <bernardogzzf@gmail.com>
2013-06-13 12:57:41 -0500

    add jquery.timeago.es to asset pipeline
    fix jquery.timeago acutes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,8 @@
 //= require jquery_ujs
 //= require jquery_nested_form
 //= require jquery.ui.datepicker
-//= require jquery-timeago/jquery.timeago
+//= require jquery.timeago
+//= require jquery-timeago/locales/jquery.timeago.es
 //= require jquery-fileupload/basic
 //= require highcharts
 //= require highcharts/modules/exporting
