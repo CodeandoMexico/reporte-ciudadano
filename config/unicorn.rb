77e5b87152a97ffcfd3e8372a8e5280e97a0f683
Abraham Kuri <kurenn@icalialabs.com>
2013-06-25 18:56:56 -0500

    Fix on the unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,27 +1,27 @@
 # config/unicorn.rb
 
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
-timeout Integer(ENV['WEB_TIMEOUT'] || 30)
-preload_app true
+worker_processes 3 #Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 30 #Integer(ENV['WEB_TIMEOUT'] || 30)
+#preload_app true
 
-before_fork do |server, worker|
+#before_fork do |server, worker|
 
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
-  end
+  #Signal.trap 'TERM' do
+    #puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    #Process.kill 'QUIT', Process.pid
+  #end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
-end
+  #defined?(ActiveRecord::Base) and
+    #ActiveRecord::Base.connection.disconnect!
+#end
 
-after_fork do |server, worker|
+#after_fork do |server, worker|
 
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
-  end
+  #Signal.trap 'TERM' do
+    #puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+  #end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
-end
+  #defined?(ActiveRecord::Base) and
+    #ActiveRecord::Base.establish_connection
+#end
 
