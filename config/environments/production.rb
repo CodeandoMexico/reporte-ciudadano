08de720326ab3f535393423c515ee16ae9dab1ae
Abraham Kuri <kurenn@icalialabs.com>
2013-08-19 14:17:55 -0500

    Default url options for sending emails on production added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,8 @@ ReporteCiudadano::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  #
+  config.action_mailer.default_url_options = { :host => 'tuadosapaco.herokuapp.com' }
   
   #Carrierwave config
   CarrierWave.configure do |config|
