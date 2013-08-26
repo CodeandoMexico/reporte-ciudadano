2ed78277d7d681c6f6149b7449cbd657f8b0562a
Abraham Kuri <kurenn@icalialabs.com>
2013-08-26 14:04:24 -0500

    Change reports filtered by status with the status id

--- app/models/report.rb
+++ app/models/report.rb
@@ -27,8 +27,8 @@ class Report < ActiveRecord::Base
     where("created_at <= ?", to)
   }
 
-  scope :with_status, lambda { |status|
-    where(status: status) 
+  scope :with_status, lambda { |status_id|
+    where(status_id: status_id) 
   }
 
   scope :on_category, lambda { |category| 
@@ -40,15 +40,15 @@ class Report < ActiveRecord::Base
   }
 
   scope :closed, lambda {
-    where(status: :closed)
+    where(status_id: 4)
   }
 
   scope :not_closed, lambda {
-    where('status != ?', :closed)
+    where('status_id != ?', 4)
   }
 
   scope :open, lambda {
-    where(status: :open)
+    where(status: 1)
   }
 
   acts_as_voteable
@@ -67,7 +67,7 @@ class Report < ActiveRecord::Base
     reports = Report.order('created_at')
     reports = reports.on_start_date(params[:start_date]) unless params[:start_date].blank?
     reports = reports.on_finish_date(params[:end_date]) unless params[:end_date].blank?
-    reports = reports.with_status(params[:status]) unless params[:status].blank?
+    reports = reports.with_status(params[:status_id]) unless params[:status_id].blank?
     reports = reports.on_category(params[:category_id]) unless params[:category_id].blank?
     reports = reports.find_by_ids(params[:report_ids]) unless params[:report_ids].blank?
     reports
