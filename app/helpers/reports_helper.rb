2fd7d6e26639e90d38de9bd2c78a957699630797
Edilberto Ruvalcaba <eddie.ruva@gmail.com>
2013-09-02 11:53:22 -0500

    Fixes styling for root dashboard stats and reports view
    Adds new column implementation to report index
    
    Fixes bad styling with borders in reports stats
    
    Fixes summary report map css
    
    Fixes responsive layout in recent reports list
    
    Fixes layout responsive for reports stats in summary
    
    Fixes problem with min height for body
    
    Removes highcharts title
    
    Styles for reports views
    Fixes search filters in reports index
    
    Adds bootstrap style to report index table
    
    Adds bootstrap styling to new report form
    
    Adds bootstrap styles to show report view

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -6,13 +6,13 @@ module ReportsHelper
 
   def vote_link_for(report)
     if user_signed_in? && !current_user.voted_on?(report)
-      link_to 'VOTA', vote_report_path(report), :remote => true, :method => :post, :class => "js-vote_report", class: "button blue rounded"
+      link_to 'VOTA', vote_report_path(report), :remote => true, :method => :post, :class => "js-vote_report", class: "btn btn-primary btn-block"
     elsif user_signed_in? && current_user.voted_on?(report)
-      link_to 'VOTASTE', "javascript:void(0)", class: "button green"
+      link_to 'VOTASTE', "javascript:void(0)", class: "btn btn-success btn-block"
     elsif admin_signed_in?
-      link_to 'VOTA', "javascript:void(0)", { class: "button green blocked", data: { message: "Los administradores no pueden votar." } }
+      link_to 'VOTA', "javascript:void(0)", { class: "btn btn-primary blocked btn-block", data: { message: "Los administradores no pueden votar." } }
     else
-      link_to 'VOTA', "javascript:void(0)", { class: "button green blocked", data: { message: "Para votar necesitas registrarte." } }
+      link_to 'VOTA', "javascript:void(0)", { class: "btn btn-primary blocked btn-block", data: { message: "Para votar necesitas registrarte." } }
     end
   end
 
