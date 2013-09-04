de65ead6126678012b276f22d4e86917fa8aa5b7
Edilberto Ruvalcaba <eddie.ruva@gmail.com>
2013-09-03 22:56:30 -0500

    Fixes responsive navbar, it was missing a js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,6 +6,7 @@
 //= require jquery-timeago/locales/jquery.timeago.es
 //= require jquery-fileupload/basic
 //= require twitter/bootstrap/dropdown
+//= require twitter/bootstrap/collapse
 //= require highcharts
 //= require highcharts/modules/exporting
 //= require jquery.tablesorter
