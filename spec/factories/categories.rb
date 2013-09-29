597e8eaf939d63ee66bd2d09689b2f90e6e56c81
Edilberto Ruvalcaba <eddie.ruva@gmail.com>
2013-09-29 16:53:20 -0500

    Adds model tests for category model

---
+++ spec/factories/categories.rb
@@ -0,0 +1,12 @@
+require 'ffaker'
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :category do
+    name { Faker::Lorem.words(2) }
+
+    factory :invalid_category do
+      name nil
+    end
+  end
+end
