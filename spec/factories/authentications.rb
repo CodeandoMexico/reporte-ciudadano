5cedcb826d0b16229a54879ad16b4a10f123c085
Edilberto Ruvalcaba <eddie.ruva@gmail.com>
2013-09-29 15:34:01 -0500

    Adds model tests for authentication

---
+++ spec/factories/authentications.rb
@@ -0,0 +1,10 @@
+require 'ffaker'
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :authentication do
+    user
+    uid '12345678'
+    provider 'facebook'
+  end
+end
