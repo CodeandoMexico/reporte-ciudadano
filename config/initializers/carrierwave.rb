8dd5a1b87dc821599a7661c5a4aad002f2769024
abisosa <abigail.sosa.h@gmail.com>
2015-03-11 18:37:43 -0600

    Exclude fog credentials from test environment

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,16 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => ENV['AWS_KEY'],
-    :aws_secret_access_key  => ENV['AWS_SECRET']
-  }
-  config.fog_directory  = ENV['S3_BUCKET']
+  unless Rails.env.test?
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['AWS_KEY'],
+      :aws_secret_access_key  => ENV['AWS_SECRET']
+    }
+    config.fog_directory  = ENV['S3_BUCKET']
+  end
   config.fog_public     = true
   # config.root      = Rails.root.join('tmp')
   # config.cache_dir = 'files'
-
   config.permissions = 0600
-
   config.storage(Rails.env.test? || Rails.env.development? ? :file : :fog)
   config.enable_processing = false if Rails.env.test?
 end
