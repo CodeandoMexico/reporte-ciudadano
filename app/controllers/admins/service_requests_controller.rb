f317b7bc4feb31a78ff70c22fc60130661078966
abisosa <abigail.sosa.h@gmail.com>
2015-03-19 15:19:46 -0600

    Added default value for service_field param

--- app/controllers/admins/service_requests_controller.rb
+++ app/controllers/admins/service_requests_controller.rb
@@ -47,7 +47,7 @@ class Admins::ServiceRequestsController < Admins::AdminController
   def service_request_params
     service_fields = params[:service_request].delete(:service_fields)
     params.require(:service_request).permit(:address, :status_id, :service_id, :description, :media, :anonymous, :lat, :lng).tap do |whitelisted|
-      whitelisted[:service_fields] = service_fields
+      whitelisted[:service_fields] = service_fields || {}
     end
   end
 end
