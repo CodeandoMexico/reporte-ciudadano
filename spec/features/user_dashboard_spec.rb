75771076addcfd824f6da6ef45a9cb9d1154509a
abisosa <abigail.sosa.h@gmail.com>
2015-03-19 18:03:17 -0600

    Fix user dashboard specs

--- spec/features/user_dashboard_spec.rb
+++ spec/features/user_dashboard_spec.rb
@@ -12,7 +12,9 @@ feature 'When I am in the dashboard' do
 
     scenario 'I can vote on an service_request', js: true do
       visit root_path
-      page.find("a[href='/service_requests/#{service_requests.first.id}/vote']").click
+      within first(".js-vote_status") do
+        click_link "VOTA"
+      end
       expect(page).to have_content('Votaste')
     end
   end
