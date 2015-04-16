ae45686059c27bceb617bdff4204cc747455a5e8
Abigail Sosa <abigail.sosa.h@gmail.com>
2015-04-16 17:33:12 -0500

    Fix edit public servant spec

--- spec/features/admin/service_admin_can_edit_public_servant_spec.rb
+++ spec/features/admin/service_admin_can_edit_public_servant_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 feature 'As a service admin I can edit a public servant' do
 
-  let(:admin) { create(:admin, :service_admin) }
+  let(:admin) { create(:admin, :service_admin, dependency: "Dependencia 1") }
 
   background do
     sign_in_admin admin
   end
 
   scenario 'I can edit a public servant' do
-    public_servants = create_list(:admin, 3, :public_servant)
+    public_servants = create_list(:admin, 3, :public_servant, dependency: admin.dependency)
     first_public_servant = public_servants.first
 
     visit admins_dashboards_path
@@ -19,13 +19,11 @@ feature 'As a service admin I can edit a public servant' do
     expect(find_field('admin[name]').value).to eq first_public_servant.name
 
     fill_in "admin[name]", with: "New name"
-    select "Dependencia 2", from: "admin[dependency]"
     select "Unidad administrativa 2", from: "admin[administrative_unit]"
     click_button "Actualizar"
 
     expect(page).to have_content "El servidor público se ha actualizado exitosamente"
     expect(page).to have_content "New name"
-    expect(page).to have_content "Dependencia 2"
     expect(page).to have_content "Unidad administrativa 2"
   end
 
