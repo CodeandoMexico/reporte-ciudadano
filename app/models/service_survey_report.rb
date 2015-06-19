86ca1e6c03d8acdbb11869d746668b41b4190d1f
poguez <zeugop@gmail.com>
2015-06-19 17:00:56 -0500

    Se agregan nuevos metodos para calcular el reporte y se refactoriza el codigo entero, ya se calculan personas participantes en el survey.

--- app/models/service_survey_report.rb
+++ app/models/service_survey_report.rb
@@ -1,38 +1,36 @@
 class ServiceSurveyReport < ActiveRecord::Base
   belongs_to :service_survey
-
   before_create :get_results_for_survey
 
-  def get_results_for_survey(id)
-    positive_overall_perception = self.report(id)[:survey_results][:positive]
-    negative_overall_perception = self.report(id)[:survey_results][:negative]
-
+  def get_results_for_survey
+    self.positive_overall_perception = report(self.service_survey_id)[:survey][:positive]
+    self.negative_overall_perception = report(self.service_survey_id)[:survey][:negative]
+    self.people_who_participated = report(self.service_survey_id)[:people_count]
   end
 
-  private
-
-  def self.report(id)
-     survey_results = effectiveness_survey(id)
+  def report(service_survey_id)
+     survey_results = effectiveness_survey(service_survey_id)
      {}.merge(survey_results)
   end
 
-  def self.people_who_participated_in_survey(id)
-
-  end
-
-  def self.effectiveness_survey(id)
-    {:survey => {:positive =>"#{overall_effectiveness(id)}",
-                 :negative => "#{ 100 - overall_effectiveness(id)}"},
-     :title => ServiceSurvey.find(id).title
+  def effectiveness_survey(service_survey_id)
+    {:survey => {:positive =>"#{overall_effectiveness(service_survey_id)}",
+                 :negative => "#{ 100 - overall_effectiveness(service_survey_id)}"},
+     :title => ServiceSurvey.find(service_survey_id).title,
+     :people_count => get_service_survey(service_survey_id).answers.select(:user_id).distinct.count
     }
   end
 
-  def self.overall_effectiveness(id)
-    rating_answers(id).map(&:score).sum.to_i
+  def overall_effectiveness(service_survey_id)
+    rating_answers(service_survey_id).map(&:score).sum.to_i
   end
 
-  def self.rating_answers(id)
-    ServiceSurvey.find(id).rating_answers
+  def rating_answers(service_survey_id)
+    get_service_survey(service_survey_id).rating_answers
+  end
+
+  def get_service_survey(service_survey_id)
+    ServiceSurvey.find(service_survey_id)
   end
 
 end
