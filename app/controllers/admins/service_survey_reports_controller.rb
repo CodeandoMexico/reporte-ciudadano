66f4020353433dc4578e004c36732595006636de
poguez <zeugop@gmail.com>
2015-06-24 23:44:30 -0500

    Se corrigen locales para los reportes, habia un error donde uno decia se ha generado la encuesta.

--- app/controllers/admins/service_survey_reports_controller.rb
+++ app/controllers/admins/service_survey_reports_controller.rb
@@ -4,10 +4,9 @@ class Admins::ServiceSurveyReportsController < ApplicationController
   def create
     @service_survey_report = ServiceSurveyReport.new(service_survey_report_params)
     if @service_survey_report.save
-      redirect_to  service_survey_report_path(@service_survey_report), notice: t('flash.service_survey.created')
+      redirect_to  service_survey_report_path(@service_survey_report), notice: t('service_report.created')
     else
-      flash[:error] = t('service_report.no_reports')
-      redirect_to  admins_service_surveys_path
+      redirect_to  admins_service_surveys_path, notice: t('service_report.no_reports')
     end
   end
 
