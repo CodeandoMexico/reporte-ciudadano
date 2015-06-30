ed8547b972dd03fd22a561234b63d25a46928d71
Abigail Sosa <abigail.sosa.h@gmail.com>
2015-06-30 00:07:00 -0600

    Rename service reports association to reports

--- app/services/evaluations.rb
+++ app/services/evaluations.rb
@@ -37,7 +37,7 @@ module Evaluations
     end
 
     def service_surveys_reports
-      service_surveys.map(&:service_survey_reports).flatten
+      service_surveys.map(&:reports).flatten
     end
 
     private
