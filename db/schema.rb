02ef56cda9e4f3a1f2a7e07d84572fea6802d915
poguez <zeugop@gmail.com>
2015-06-30 17:03:11 -0500

    Se agrega al schema la referencia de id del servicio evaluado y el soporte para hstore

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150619174124) do
+ActiveRecord::Schema.define(version: 20150630173307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
@@ -159,8 +160,7 @@ ActiveRecord::Schema.define(version: 20150619174124) do
     t.float    "positive_overall_perception", default: 0.0, null: false
     t.float    "negative_overall_perception", default: 0.0, null: false
     t.integer  "people_who_participated",     default: 0,   null: false
-    t.string   "phase",                                     null: false
-    t.string   "title",                                     null: false
+    t.integer  "service_id",                                null: false
     t.datetime "created_at",                                null: false
     t.datetime "updated_at",                                null: false
   end
