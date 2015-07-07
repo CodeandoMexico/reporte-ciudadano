1af3f0e6186f6f08068dcea5534ee54524583756
Miguel Angel Gordian <os.aioria@gmail.com>
2015-07-07 18:53:50 -0500

    Sidekiq reconoce las ENVs de un contenedor de redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,10 @@
 if Rails.env.production?
   Sidekiq.configure_server do |config|
-    config.redis = { url: ENV["REDISCLOUD_URL"], size: 3 }
+    if ENV['REDIS_PORT_6379_TCP_ADDR'] and ENV[''REDIS_PORT_6379_TCP_PORT'']
+      config.redis = { url: "redis://#{ENV['REDIS_PORT_6379_TCP_ADDR']}:#{ENV['REDIS_PORT_6379_TCP_PORT']}/0", namespace: 'Tyresearch' }
+    else
+      config.redis = { url: ENV["REDISCLOUD_URL"], size: 3 }
+    end
 
     database_url = ENV['DATABASE_URL']
     if database_url
@@ -13,4 +17,3 @@ if Rails.env.production?
     config.redis = {size: 1}
   end
 end
-
