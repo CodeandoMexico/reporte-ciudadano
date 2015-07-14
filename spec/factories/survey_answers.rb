480745daf445b543740005cbbd1a918f04eb9dc6
poguez <zeugop@gmail.com>
2015-07-14 11:13:26 -0500

    Se agrega tipo de respuesta binaria

--- spec/factories/survey_answers.rb
+++ spec/factories/survey_answers.rb
@@ -1,9 +1,15 @@
 FactoryGirl.define do
   factory :survey_answer do
     text "MyString"
-question nil
-score "9.99"
-user nil
+    question nil
+    score "9.99"
+    user nil
+
+    factory :survey_answer_binary do
+      score 1
+      user nil
+      question nil
+    end
   end
 
 end
