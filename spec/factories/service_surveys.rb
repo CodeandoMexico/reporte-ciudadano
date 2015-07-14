61db1a8c0d0034fa31a72c0a2424b23987d43882
poguez <zeugop@gmail.com>
2015-07-14 11:14:23 -0500

    Se agrega factory para encuestas con preguntas binarias y respuestas

--- spec/factories/service_surveys.rb
+++ spec/factories/service_surveys.rb
@@ -10,6 +10,17 @@ FactoryGirl.define do
         create :question, :binary, value: 100.0, service_survey: survey
         survey.reload
       end
+      factory :survey_with_binary_question_and_answers do
+        transient do
+          answers_count 5
+          questions_count 1
+        end
+        after(:create) do |survey, evaluator|
+          create_list(:question, evaluator.questions_count, service_survey: survey).each do |q|
+            create_list(:survey_answer_binary,  evaluator.answers_count, question: q)
+          end
+        end
+      end
     end
   end
 end
