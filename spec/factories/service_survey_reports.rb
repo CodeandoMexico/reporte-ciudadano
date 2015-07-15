209104155c1aa995553fdb2a8a315a3994ddd964
poguez <zeugop@gmail.com>
2015-07-15 16:45:27 -0500

    Se agrega un nuevo factory que es un reporte con una encuesta con 100% de satisfacción.

--- spec/factories/service_survey_reports.rb
+++ spec/factories/service_survey_reports.rb
@@ -3,15 +3,24 @@ FactoryGirl.define do
     service_survey_id nil
     association :service_survey, factory: :survey_with_binary_question
 
+    factory :valid_service_survey_report do
+      before(:create) do |report|
+        report.service_survey_id = FactoryGirl.create(:survey_with_binary_question_and_answers).id
+      end
+    end
 
-    before(:create) do |report|
-      report.service_survey_id = FactoryGirl.create(:survey_with_binary_question_and_answers).id
+    factory :valid_service_survey_report_100 do
+      before(:create) do |report|
+        report.service_survey_id = FactoryGirl.create(:survey_with_binary_rating_questions_and_answers).id
+      end
+
     end
 
     factory :invalid_service_survey_report do
       service_survey_id nil
       association :service_survey, factory: :survey_with_binary_question
     end
+
   end
 
 end
