2f9527490aea66021d5b98f61f79c563670e93af
poguez <zeugop@gmail.com>
2015-07-15 16:47:21 -0500

    Se agregan factories para tipos de respuestas posibles de binaria y de rating.

--- spec/factories/survey_answers.rb
+++ spec/factories/survey_answers.rb
@@ -5,9 +5,45 @@ FactoryGirl.define do
     score "9.99"
     user nil
     factory :survey_answer_binary do
-      score 1
+      score nil
       user nil
       question nil
     end
+    factory :survey_answer_rating do
+      score nil
+      user nil
+      question nil
+    end
+
+    factory :survey_answer_binary_yes, traits:[:yes_answer]
+    factory :survey_answer_binary_no, traits:[:no_answer]
+    factory :survey_answer_rating_100, traits:[:hundred_percent]
+    factory :survey_answer_rating_75, traits: [:seventy_five_percent]
+    factory :survey_answer_rating_50, traits:[:fifty_percent]
+    factory :survey_answer_rating_25, traits: [:twenty_five_percent]
+    factory :survey_answer_rating_0, traits: [:zero_percent]
+
+    trait :hundred_percent do
+      score 1
+    end
+    trait :seventy_five_percent do
+      score 0.75
+    end
+    trait :fifty_percent do
+      score 0.5
+    end
+    trait :twenty_five_percent do
+      score 0.25
+    end
+    trait :zero_percent do
+      score 0
+    end
+    trait :no_answer do
+      score 0
+    end
+    trait :yes_answer do
+      score 1
+    end
+
   end
 end
