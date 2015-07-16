152f19b71ef89084db2fe18aa54ad6c2689389ba
Miguel Angel Gordian <os.aioria@gmail.com>
2015-07-16 04:36:30 -0500

    Uso de strings en vez de symbols

--- Deploy/site-cookbooks/urbem/recipes/default.rb
+++ Deploy/site-cookbooks/urbem/recipes/default.rb
@@ -2,33 +2,43 @@
 # Cookbook Name:: urbem
 # Recipe:: default
 #
-# Copyright 2015, YOUR_COMPANY_NAME
+# Copyright 2015, Cívica Digital
 #
-# All rights reserved - Do Not Redistribute
+# AGPL License
 #
+
 chef_gem 'docker-api'
 require 'docker'
 
-secret = Chef::EncryptedDataBagItem.load_secrets("#{node[:key_file][:path]}")
-creds = Chef::EncryptedDataBagItem.load("urbem", "keys", secret)
+include_recipe 'git'
+
+if node["key_file"]
+  secret = Chef::EncryptedDataBagItem.load_secrets("#{node["key_file"]["path"]}")
+  creds = Chef::EncryptedDataBagItem.load("urbem", "keys", secret)
+else
+  creds = data_bag_item('keys', 'secret')
+end
+
+
 
 list_creds = [
-  "MAILER_FROM=#{creds[:email]}",
-  "FACEBOOK_SECRET=#{creds[:facebook][:secret]}",
-  "FACEBOOK_KEY=#{creds[:facebook][:key]}",
-  "TWITTER_KEY=#{creds[:twitter][:key]}",
-  "TWITTER_SECRET=#{creds[:twitter][:secret]}",
-  "TWITTER_OAUTH_TOKEN=#{creds[:twitter][:oauth_token]}",
-  "TWITTER_OAUTH_TOKEN_SECRET=#{creds[:twitter][:oauth_token_secret]}",
-  "SENDGRID_USERNAME=#{creds[:sendgrid][:username]}",
-  "SENDGRID_PASSWORD=#{creds[:sendgrid][:password]}",
-  "COVERALLS_TOKEN=#{creds[:coverall_token]}",
-  "SECRET_KEY_BASE=#{creds[:rails][:secret_key_base]}",
-  "RAILS_ENV=#{creds[:rails][:env]}",
-  "POSTGRES_PASSWORD=#{creds[:postgres][:password]}",
-  "S3_BUCKET=#{creds[:aws][:s3_bucket_name]}",
-  "AWS_SECRET=#{creds[:aws][:aws_secret]}",
-  "AWS_KEY=#{creds[:aws][:aws_key]}"
+  "MAILER_FROM=#{creds['email']}",
+  "FACEBOOK_SECRET=#{creds['facebook']['secret']}",
+  "FACEBOOK_KEY=#{creds['facebook']['key']}",
+  "TWITTER_KEY=#{creds['twitter']['key']}",
+  "TWITTER_SECRET=#{creds['twitter']['secret']}",
+  "TWITTER_OAUTH_TOKEN=#{creds['twitter']['oauth_token']}",
+  "TWITTER_OAUTH_TOKEN_SECRET=#{creds['twitter']['oauth_token_secret']}",
+  "SENDGRID_USERNAME=#{creds['sendgrid']['username']}",
+  "SENDGRID_PASSWORD=#{creds['sendgrid']['password']}",
+  "COVERALLS_TOKEN=#{creds['coverall_token']}",
+  "SECRET_KEY_BASE=#{creds['rails']['secret_key_base']}",
+  "RAILS_ENV=#{creds['rails']['env']}",
+  "PASSENGER_APP_ENV=#{creds['rails']['env']}",
+  "POSTGRES_PASSWORD=#{creds['postgres']['password']}",
+  "S3_BUCKET=#{creds['aws']['s3_bucket_name']}",
+  "AWS_SECRET=#{creds['aws']['aws_secret']}",
+  "AWS_KEY=#{creds['aws']['aws_key']}"
 ]
 
 # Up the docker service
