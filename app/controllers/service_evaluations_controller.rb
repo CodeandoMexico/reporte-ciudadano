2fa89b1a58ba33172ddd14d6c9b58affc2024483
Abigail Sosa <abigail.sosa.h@gmail.com>
2015-07-22 13:08:14 -0600

    Added authentication for evaluations controllers

--- app/controllers/service_evaluations_controller.rb
+++ app/controllers/service_evaluations_controller.rb
@@ -1,4 +1,6 @@
 class ServiceEvaluationsController < ApplicationController
+  before_action :authenticate_user!
+
   def show
     @service = Service.includes(:service_surveys, :questions, :answers).find(params[:id])
     requested_survey = @service.service_surveys.find_by_id(params[:service_survey_id])
