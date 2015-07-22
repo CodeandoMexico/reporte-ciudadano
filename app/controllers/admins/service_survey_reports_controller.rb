9e5c98e8ee252639bfc917d31c2f62e739444575
Abigail Sosa <abigail.sosa.h@gmail.com>
2015-07-22 17:35:28 -0600

    Added cis and service reports generation on create action

--- app/controllers/admins/service_survey_reports_controller.rb
+++ app/controllers/admins/service_survey_reports_controller.rb
@@ -3,7 +3,9 @@ class Admins::ServiceSurveyReportsController < ApplicationController
 
   def create
     @service_survey_report = ServiceSurveyReport.new(service_survey_report_params)
+
     if @service_survey_report.save
+      generate_services_and_cis_reports
       redirect_to  service_survey_report_path(@service_survey_report), notice: t('service_report.created')
     else
       redirect_to  admins_service_surveys_path, notice: t('service_report.no_reports')
