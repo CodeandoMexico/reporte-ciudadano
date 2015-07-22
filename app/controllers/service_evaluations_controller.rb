7e28fa45d119707400c9370858dabfcac5283fb0
Abigail Sosa <abigail.sosa.h@gmail.com>
2015-07-22 12:55:01 -0600

    Added pagination to respondents on service evaluations

--- app/controllers/service_evaluations_controller.rb
+++ app/controllers/service_evaluations_controller.rb
@@ -3,5 +3,6 @@ class ServiceEvaluationsController < ApplicationController
     @service = Service.includes(:service_surveys, :questions, :answers).find(params[:id])
     requested_survey = @service.service_surveys.find_by_id(params[:service_survey_id])
     @service_survey = requested_survey || @service.service_surveys.last
+    @respondents = Kaminari.paginate_array(@service_survey.respondents).page(params[:page]).per(20)
   end
 end
