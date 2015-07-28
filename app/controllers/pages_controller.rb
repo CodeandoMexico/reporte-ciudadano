bcf0a538139a4e38d2af00e4b3d17561ed10142f
Abigail Sosa <abigail.sosa.h@gmail.com>
2015-07-28 10:29:29 -0600

    Fix public servants count

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def index
     @services_count = Service.count
     @active_citizen_count = SurveyAnswer.pluck(:user_id).uniq.count
-    @public_servant_assessed_count = Question.with_public_servant_type.map(&:services).flatten.uniq.map(&:admins).uniq.count
+    @public_servant_assessed_count = Question.with_public_servant_type.map(&:services).flatten.map(&:admins).flatten.uniq.count
     @url_video = 'https://www.youtube.com/watch?v=uFLWkxsUFr8'
   end
 end
