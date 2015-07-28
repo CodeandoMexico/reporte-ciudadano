eb9fd3dcbda7fca756e1cc86f44ca3f3d9e9dcf1
Abigail Sosa <abigail.sosa.h@gmail.com>
2015-07-27 22:11:34 -0600

    Added public servants count KPI

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,8 @@
 class PagesController < ApplicationController
   def index
-    @service_requests = ServiceRequest.filter_by_search(params).page(params[:page])
-    @open_service_requests = ServiceRequest.not_closed.count
-    @closed_service_requests = ServiceRequest.closed.count
-    @all_service_requests = ServiceRequest.count
-    @chart_data = Service.chart_data.to_json
-    @status_data = Status.select(:name, :id).to_json
-    flash.now[:notice] = "No se encontraron solicitudes de servicio." if @service_requests.empty?
     @services_count = Service.count
-    @active_citizen_count = SurveyAnswer.select(:user_id).uniq.count
-    @public_servant_assessed_count = 0
+    @active_citizen_count = SurveyAnswer.pluck(:user_id).uniq.count
+    @public_servant_assessed_count = Question.with_public_servant_type.map(&:services).flatten.uniq.map(&:admins).uniq
     @url_video = 'https://www.youtube.com/watch?v=uFLWkxsUFr8'
   end
 end
