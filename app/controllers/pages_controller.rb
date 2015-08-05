1d4a00241a7517a754f30ad5f8e4b8fe27047e4d
poguez <zeugop@gmail.com>
2015-08-04 20:31:40 -0500

    El id del video de vimeo de la pagina principal se toma de una variable de entorno y tiene un id del video provisional como fallback

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,6 @@ class PagesController < ApplicationController
     @services_count = Service.count
     @active_citizen_count = SurveyAnswer.pluck(:user_id).uniq.count
     @public_servant_assessed_count = Question.with_public_servant_type.map(&:services).flatten.map(&:admins).flatten.uniq.count
-    @url_video = '134171429'
+    @url_video = ENV['VIMEO_VIDEO_KEY'] || '134171429'
   end
 end
