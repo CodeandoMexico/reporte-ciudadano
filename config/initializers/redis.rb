3e49f9c5dcd6d18fcd427e6d7f7b0b8e544926c2
Miguel Angel Gordian <os.aioria@gmail.com>
2015-08-07 03:48:00 -0500

    Arregla el error de conexión con Redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,13 @@
 if Rails.env.production?
-  uri = URI.parse(ENV["REDISTOGO_URL"])
-  REDIS = Redis.new(:url => uri)
+  uri_s = ENV["REDISTOGO_URL"]
+
+  if not uri_s and ENV["REDIS_PORT_6379_TCP_ADDR"] and ENV['REDIS_PORT_6379_TCP_PORT']
+    container_host = ENV['REDIS_PORT_6379_TCP_ADDR']
+    container_port = ENV['REDIS_PORT_6379_TCP_PORT']
+
+    uri_s = "redis://#{container_host}:#{container_port}"
+  end
+
+  uri_o = URI.parse(uri_s)
+  REDIS = Redis.new(:url => uri_o)
 end
