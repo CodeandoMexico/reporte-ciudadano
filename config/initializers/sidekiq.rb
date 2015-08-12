ed4179df7d75076ff44e66e95089ef0084dbe177
Miguel Angel Gordian <os.aioria@gmail.com>
2015-08-12 06:33:45 -0500

    Agrega la conexión a redis al cliente de sidekiq.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,18 @@
 if Rails.env.production?
-  Sidekiq.configure_server do |config|
+  def docker_redis_url
     if ENV['REDIS_PORT_6379_TCP_ADDR'] and ENV['REDIS_PORT_6379_TCP_PORT']
-      config.redis = { url: "redis://#{ENV['REDIS_PORT_6379_TCP_ADDR']}:#{ENV['REDIS_PORT_6379_TCP_PORT']}/0", size: 3 }
+      return "redis://#{ENV['REDIS_PORT_6379_TCP_ADDR']}:#{ENV['REDIS_PORT_6379_TCP_PORT']}/0"
+    end
+    nil
+  end
+
+  Sidekiq.configure_server do |config|
+    if docker_redis_url
+      config.redis = { url: docker_redis_url, size: 3 }
     else
       config.redis = { url: ENV["REDISCLOUD_URL"], size: 3 }
     end
+
 
     database_url = ENV['DATABASE_URL']
     if database_url
@@ -14,6 +22,10 @@ if Rails.env.production?
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = {size: 1}
+    if docker_redis_url
+      config.redis = { url: docker_redis_url, size: 3 }
+    else
+      config.redis = { size: 3 }
+    end
   end
 end
