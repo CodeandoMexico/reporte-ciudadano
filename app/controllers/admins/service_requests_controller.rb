46e52e5d973d9f5f5cf51d3e3a0bc079ccfa4821
mikesaurio <mikesaurio@gmail.com>
2015-08-18 19:55:45 -0500

    se elimina radiobutton para enviar mensage custome

--- app/controllers/admins/service_requests_controller.rb
+++ app/controllers/admins/service_requests_controller.rb
@@ -30,7 +30,6 @@ class Admins::ServiceRequestsController < Admins::AdminController
 
   def edit
     @service_request = ServiceRequest.find params[:id]
-    @messages = @service_request.service.messages.with_status(@service_request.status_id)
     @comments = @service_request.comments.order("comments.created_at ASC")
     @admins_services = Service.where(id: @service_request.service.id).last.admins
   end
