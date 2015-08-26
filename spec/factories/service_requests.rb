19925dd567276af217abf2f44ec4b14d16371128
mikesaurio <mikesaurio@gmail.com>
2015-08-26 13:18:08 -0500

    WIP se cambia service_survey y service model

--- spec/factories/service_requests.rb
+++ spec/factories/service_requests.rb
@@ -4,6 +4,10 @@ FactoryGirl.define do
   factory :service_request do
     service
     status
+    cis [1]
+    service_admin_id FactoryGirl.build(:admin,id: 1)
+    public_servant_id FactoryGirl.build(:admin, :service_admin, id: 1, is_public_servant: 1)
+    byebug
     association :requester, factory: :user
     address     { FFaker::Lorem.words(3) }
     description { FFaker::Lorem.paragraph(2) }
--- spec/factories/services.rb
+++ spec/factories/services.rb
@@ -4,11 +4,14 @@ FactoryGirl.define do
   factory :service do
     sequence(:name) { |n| "Service # #{n}" }
     status "activo"
-
+    service_type "1"
+    dependency "La dependencia"
+    administrative_unit "unidad administrativa"
+    cis [1]
+    service_admin_id FactoryGirl.build(:admin,id: 1)
     factory :invalid_service do
       name nil
     end
-
     factory :service_with_service_fields do
       after(:create) do |service|
         service.service_fields << FactoryGirl.build(:service_field)
