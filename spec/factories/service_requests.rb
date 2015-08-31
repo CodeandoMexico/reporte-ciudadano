12a77468ea2d19777280d27f804b41254c95cf4c
poguez <zeugop@gmail.com>
2015-08-31 02:12:48 -0500

    Se corrige factory de service_request pues hacia fallar las pruebas con un atributo que no tiene el objeto

--- spec/factories/service_requests.rb
+++ spec/factories/service_requests.rb
@@ -5,8 +5,7 @@ FactoryGirl.define do
     service
     status
     cis [1]
-    service_admin_id FactoryGirl.build(:admin,id: 1)
-    public_servant_id FactoryGirl.build(:admin, :service_admin, id: 1, is_public_servant: 1)
+    public_servant_id FactoryGirl.build(:admin, :public_servant)
     association :requester, factory: :user
     address     { FFaker::Lorem.words(3) }
     description { FFaker::Lorem.paragraph(2) }
