7f21592e79ad5880512d5393ded164508b05c087
mikesaurio <mikesaurio@gmail.com>
2015-09-15 13:44:11 -0500

    se crea la vista para generar reportes dinamicos

--- app/controllers/admins/service_survey_reports_controller.rb
+++ app/controllers/admins/service_survey_reports_controller.rb
@@ -1,6 +1,8 @@
 class Admins::ServiceSurveyReportsController < ApplicationController
   layout 'admins'
-
+  helper_method :service_cis_options
+  helper_method :criterion_options
+  #Metodo que se utiliza al crear reportes desde la vista de observador
   def create
     @service_survey_report = ServiceSurveyReport.new(service_survey_report_params)
 
@@ -12,6 +14,10 @@ class Admins::ServiceSurveyReportsController < ApplicationController
     end
   end
 
+  def make_report
+
+  end
+
   private
 
   def generate_services_and_cis_reports
@@ -20,5 +26,12 @@ class Admins::ServiceSurveyReportsController < ApplicationController
 
   def service_survey_report_params
     params.require(:service_survey_report).permit(:service_survey_id, :service_id)
+  end
+  def service_cis_options
+    Services.service_cis_options
+  end
+  
+  def criterion_options
+    ServiceSurveys.criterion_options
   end
 end
