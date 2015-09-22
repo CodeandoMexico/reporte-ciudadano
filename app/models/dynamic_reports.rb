e7afd5205ed5f2cd316de525fb6a6ca30011e7d6
poguez <zeugop@gmail.com>
2015-09-22 17:22:34 -0500

    Se ajusta reporte dinamico para filtrado de reportes de respuestas de encuestas.

--- app/models/dynamic_reports.rb
+++ app/models/dynamic_reports.rb
@@ -2,26 +2,36 @@ class DynamicReports
   include Datagrid
 
   scope do
-    SurveyAnswer.rating_and_binary
+    ServiceSurveyReport.includes(:service_survey).includes(:services)
   end
 
-  # filter(:id, :integer, :multiple => true))
-  # filter(:text)
-  # filter(:score)
-  # filter(:user_id)
-  # filter(:ignored)
-  # filter(:cis_id)
-  filter(:id, :integer)
-  filter(:created_at, :date, :range => true)
+  column(:service_id, header: I18n.t('activerecord.attributes.dynamic_reports.service_id'))
+  column(:title, header: I18n.t('activerecord.attributes.dynamic_reports.title')) do |record|
+    record.service_survey_title
+  end
+  column(:is_open, header: I18n.t('activerecord.attributes.dynamic_reports.is_open')) do |record|
+    I18n.t(".#{record.service_survey.open}")
+  end
+  column(:phase, header: I18n.t('activerecord.attributes.dynamic_reports.phase')) do |record|
+    I18n.t("service_survey_phase_options.#{record.service_survey_phase}")
+  end
+  column(:people_who_participated, header: I18n.t('activerecord.attributes.dynamic_reports.people_who_participated'))
+  column(:positive_overall_perception, header: I18n.t('activerecord.attributes.dynamic_reports.positive_overall_perception'))
+  column(:negative_overall_perception, header: I18n.t('activerecord.attributes.dynamic_reports.negative_overall_perception'))
+  column(:people_who_participated, header: I18n.t('activerecord.attributes.dynamic_reports.people_who_participated'))
+  column(:created_at, header: I18n.t('activerecord.attributes.dynamic_reports.created_at')) do |record|
+    record.created_at.to_date
+  end
 
-  column(:id)
-  column(:text)
-  column(:score)
-  column(:user_id)
-  column(:ignored)
-  column(:cis_id)
-  column(:created_at) do |model|
-    model.created_at.to_date
+  column(:areas_results, header: I18n.t('activerecord.attributes.dynamic_reports.areas_results')) do |record|
+    """
+    #{I18n.t("question_criterion_options.transparency")}: #{record.areas_results[:transparency]},
+    #{I18n.t("question_criterion_options.performance")}: #{record.areas_results[:performance]},
+    #{I18n.t("question_criterion_options.quality_service")}: #{record.areas_results[:quality_service]},
+    #{I18n.t("question_criterion_options.accessibility")}: #{record.areas_results[:accessibility]},
+    #{I18n.t("question_criterion_options.infrastructure")}: #{record.areas_results[:infrastructure]},
+    #{I18n.t("question_criterion_options.public_servant")}: #{record.areas_results[:public_servant]}.
+    """
   end
 
 end
