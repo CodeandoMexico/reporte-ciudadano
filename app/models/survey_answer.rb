e4846cfd907267b4946078d50775116d7a63d8da
poguez <zeugop@gmail.com>
2015-09-22 08:16:57 -0500

    se hace join de service survey en scope de respuestas de rating

--- app/models/survey_answer.rb
+++ app/models/survey_answer.rb
@@ -6,6 +6,6 @@ class SurveyAnswer < ActiveRecord::Base
     where(ignored: false)
   }
   scope :rating_and_binary, -> {
-    SurveyAnswer.joins(:question).where("questions.answer_type" => ['rating','binary'])
+    SurveyAnswer.joins(:question).where("questions.answer_type" => ['rating','binary']).includes(:service_survey)
   }
 end
