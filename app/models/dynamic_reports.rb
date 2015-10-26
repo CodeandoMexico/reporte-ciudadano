53f926fba36ab02d2fc78fcf7c3a12385d3a662d
poguez <zeugop@gmail.com>
2015-10-25 23:15:51 -0600

    Se agrega restriccion de fechas a reporte dinamico de servidores publicos por servicio y se cambia la fecha default a un anho

--- app/models/dynamic_reports.rb
+++ app/models/dynamic_reports.rb
@@ -251,8 +251,10 @@ module DynamicReports
     filter(:created_at,
            :date,
            :range => true,
-           :default => proc { [1.month.ago.to_date, Date.today]},
-           header: I18n.t('activerecord.attributes.dynamic_reports.created_at'))
+           :default => proc { [1.year.ago.to_date, Date.today]},
+           header: I18n.t('activerecord.attributes.dynamic_reports.created_at'))  do |value, scope, grid|
+      scope.where("admins.created_at between ? and ? ", value.first + 3.days, value.last + 1.days)
+    end
 
     filter(:name,
            :enum,
