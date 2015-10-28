5df908388d01e642dfb31454dca481a1b4817a03
poguez <zeugop@gmail.com>
2015-10-28 15:29:44 -0600

    Se cambia initializer de carrierwave para poder aceptar storage local

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,5 @@
 CarrierWave.configure do |config|
-=begin
-  unless Rails.env.test?
-    config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => ENV['AWS_KEY'],
-      :aws_secret_access_key  => ENV['AWS_SECRET']
-    }
-    config.fog_directory  = ENV['S3_BUCKET']
-  end
-=end
-  config.fog_public     = true
-  config.root      = Rails.root.join('storage')
-  # config.cache_dir = "tmp"
+  config.fog_directory      = Rails.root.join('storage')
   config.permissions = 0600
   config.storage(:file)
   config.enable_processing = false if Rails.env.test?
