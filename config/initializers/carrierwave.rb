933d54cd46428538ed27da094cfdb56976a4f223
poguez <zeugop@gmail.com>
2015-10-29 15:10:22 -0600

    Se cambia initializer de carrierwave para ajustarse mejor a los uploads que se guardan en el sistema de archivos del sistema.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,10 @@
 CarrierWave.configure do |config|
-  config.fog_directory      = Rails.root.join('storage')
-  config.permissions = 0600
+  config.fog_directory      = Rails.root.join('public')
+  config.permissions = 0666
+  config.directory_permissions = 0777
   config.storage(:file)
+  config.ignore_integrity_errors = false
+  config.ignore_processing_errors = false
+  config.ignore_download_errors = false
   config.enable_processing = false if Rails.env.test?
 end
