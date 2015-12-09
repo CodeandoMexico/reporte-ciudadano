630e6d61366523eaf25f60a6cbe62bdf05a76ddb
poguez <zeugop@gmail.com>
2015-12-08 21:45:08 -0600

    Se agrega cambio en schema por agregar numero de telefono

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150910164451) do
+ActiveRecord::Schema.define(version: 20151209025931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -265,6 +265,7 @@ ActiveRecord::Schema.define(version: 20150910164451) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "telephone_number",       default: ""
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
