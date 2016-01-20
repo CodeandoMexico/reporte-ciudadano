bc27b96639cbd97adee46741663d326c466220ee
poguez <zeugop@gmail.com>
2016-01-19 18:45:01 -0600

    Se quitan estatus del seed de la base de datos, solo queda Atendido por la Dirección de atención de Quejas y Denuncias, y cerrado.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,8 @@ Service.destroy_all
 ServiceRequest.destroy_all
 Status.destroy_all
 
-open_status = Status.create(name: "Abierto", is_default: true)
-verification_status = Status.create(name: "Verificación")
-revision_status = Status.create(name: "Revisión")
+open_status = Status.create(name: "Atendido por la Dirección de atención de Quejas y Denuncias", is_default: true)
 close_status = Status.create(name: "Cerrado")
 
 super_admin = Admin.create(name: "Super admin", email: "admin@admin.com", password: "password", password_confirmation: "password", active: true)
 observer = Admin.create(name: "Observer juan", email: 'observer@observer.com', password: "password", password_confirmation: "password", is_observer: true)
-Service.all.each do |service|
-  service.messages.create(status_id: open_status.id, content: 'Mensaje para status abierto')
-  service.messages.create(status_id: verification_status.id, content: 'Mensaje para status verificado')
-  service.messages.create(status_id: revision_status.id, content: 'Mensaje para status revisado')
-  service.messages.create(status_id: close_status.id, content: 'Mensaje para status cerrado')
-end
