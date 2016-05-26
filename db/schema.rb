acded06c40619c81081b7cffa38d4923e859100e
poguez <zeugop@gmail.com>
2016-05-25 19:11:45 -0500

    Se agrega service_id a las respuestas de encuestas

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160121165906) do
+ActiveRecord::Schema.define(version: 20160525223334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -245,6 +245,7 @@ ActiveRecord::Schema.define(version: 20160121165906) do
     t.datetime "updated_at",                  null: false
     t.boolean  "ignored",     default: false
     t.integer  "cis_id",      default: -1
+    t.integer  "service_id"
   end
 
   add_index "survey_answers", ["question_id"], name: "index_survey_answers_on_question_id", using: :btree
