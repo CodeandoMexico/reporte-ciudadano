da62c4e75842f735a847f93a1e4addb4b1c7013e
Juan Carlos Mercado <barkatu.83@gmail.com>
2016-12-01 03:14:41 -0600

    Removes n+1 query in service_surveys_controller

--- app/controllers/service_surveys_controller.rb
+++ app/controllers/service_surveys_controller.rb
@@ -1,6 +1,10 @@
 class ServiceSurveysController < ApplicationController
 before_action :authenticate_user!
   def index
-    @services = Service.with_open_surveys.uniq(:id).page(params[:page]).per(20)
+    @services = Service.preload(:service_surveys)
+                       .with_open_surveys
+                       .uniq(:id)
+                       .page(params[:page])
+                       .per(20)
   end
 end
