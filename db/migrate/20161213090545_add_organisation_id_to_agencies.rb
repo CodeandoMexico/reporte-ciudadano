ad769597ee31282ff2c27bb16107385b58777e70
Abigail Sosa <abigail.sosa.h@gmail.com>
2016-12-13 16:08:20 -0600

    Fix migration file

--- db/migrate/20161213090545_add_organisation_id_to_agencies.rb
+++ db/migrate/20161213090545_add_organisation_id_to_agencies.rb
@@ -1,6 +1,5 @@
 class AddOrganisationIdToAgencies < ActiveRecord::Migration
   def change
     add_reference :agencies, :organisation, index: true
-    add_foreign_key :agencies, :organisations
   end
 end
